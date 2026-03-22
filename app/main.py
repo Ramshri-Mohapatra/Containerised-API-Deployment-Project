eb71c57a039ccbc3cd6449a308382d3fab476da2
Ramshri-Mohapatra <124039645+Ramshri-Mohapatra@users.noreply.github.com>
2026-03-22 17:40:42 +0000

    feat: add environment variables and logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+import logging
 from fastapi import FastAPI
 from app.routes import router
+from app.config import settings
 
+logging.basicConfig(
+    level=logging.DEBUG if settings.debug else logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__) 
 app = FastAPI(
     title="Task Manager API",
     description="A simple task management API",
@@ -11,4 +19,5 @@ app.include_router(router)
 
 @app.get("/")
 def root():
+    logger.info("Root endpoint was called")
     return{"message": "Task API is running"}
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+import logging
 from fastapi import APIRouter, HTTPException
 from app.models import Task
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -9,22 +12,28 @@ tasks = []
 
 @router.get("/tasks")
 def get_tasks():
+    logger.info(f"Fetching all tasks - {len(tasks)} tasks found")
     return tasks
 
 @router.post("/tasks")
 def create_tasks(task: Task):
     tasks.append(task)
+    logger.info(f"Task created: {task.title}")
     return{"message": "Task created", "task": task}
 
 @router.get("/tasks/{task_id}")
 def get_task(task_id: int):
     if task_id >= len(tasks) or task_id < 0:
+        logger.warning(f"Task {task_id} not found")
         raise HTTPException(status_code=404, detail ="Task mot found")
+    logger.info(f"Fetching task {task_id}")
     return tasks[task_id]
 
 @router.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
     if task_id >= len(tasks) or task_id < 0:
+        logger.warning(f"Attempted to delete non-existent task {task_id}")
         raise HTTPException(status_code=404, detail="Task not found")
-    tasks.pop(task_id)
+    deleted = tasks.pop(task_id)
+    logger.info(f"Task deleted: {deleted.title}")
     return {"message": "Task deleted"}
